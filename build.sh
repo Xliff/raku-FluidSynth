94c991712e5f6459134d3b6ea28cdc3f46397655
Xliff <clifton.wood@gmail.com>
2022-05-31 15:50:11 -0400

    - Adds sample soundfonts

--- build.sh
+++ build.sh
@@ -1,1 +1,36 @@
-scripts/build.sh
+#!/bin/bash
+perl6 scripts/backup_results.pl6
+echo -e "Dependency Generation\n=====================" >> LastBuildResults
+/usr/bin/time -p -o LastBuildResults -a perl6 scripts/dependencies.pl6
+if [ "$?" -ne "0" ]; then
+  exit
+fi
+
+if [ "$1" == "--start-at" ]; then
+  shift
+  re='^[0-9]+$'
+  if ! [[ $1 =~ $re ]] ; then
+    echo "Searching for $1"
+    ln=`awk "/$1/{ print NR; exit }" BuildList`
+  else
+    echo "Starting at $1"
+    ln=$1
+  fi
+  shift
+  tail --lines=+$ln BuildList > BuildList.now
+else
+  cp BuildList BuildList.now
+fi
+
+/usr/bin/time -p /bin/bash -c '(
+  i=1; n=`wc -l BuildList | cut -f1 -d\ `; for a in `cat BuildList.now`; do
+    (
+    	echo " === $a === ($i/$n)"
+	    P6_GLIB_COMPILE_PROCESS=1 ./fsexec -e "use $a" 2>&1
+    )
+    i=$((i+1))
+  done;
+  echo;
+)' 2>&1 | tee -a LastBuildResults
+cp LastBuildResults stats/LastBuildResults-`date --utc +%Y%m%d`
+rm BuildList.now
